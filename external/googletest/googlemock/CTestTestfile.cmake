d12dbfced3bd3010d6275a63481095f6f424a657
kosuke miyoshi <miyoshi@narr.jp>
2017-08-29 14:40:29 +0900

    add gtest in external

---
+++ external/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/miyoshi/Documents/cpp_space/rodent/external/googletest/googlemock
+# Build directory: /Users/miyoshi/Documents/cpp_space/rodent/external/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
