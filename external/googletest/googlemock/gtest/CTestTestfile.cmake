c5651b0168d50f50a1b6dd777adda90281b06687
kosuke miyoshi <miyoshi@narr.jp>
2017-08-30 00:44:42 +0900

    exclude CTestTestfile.cmake

--- external/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/miyoshi/Documents/cpp_space/rodent/external/googletest/googletest
-# Build directory: /Users/miyoshi/Documents/cpp_space/rodent/external/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
