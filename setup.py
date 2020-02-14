b85476aaac638e1af7bcbe656cc5ed2c4bb80837
kosuke miyoshi <miyoshi@narr.jp>
2020-02-15 04:17:30 +0900

    preparing install setup with cmake

--- setup.py
+++ setup.py
@@ -1,38 +1,118 @@
-from setuptools import setup
+from setuptools import setup, Extension
 
+import os
+from pathlib import Path
+import subprocess
+from setuptools.command.build_ext import build_ext
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name):
+        Extension.__init__(self, name, sources=[])
+
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(e.name for e in self.extensions))
+        
+        build_directory = os.path.abspath(self.build_temp)
+
+        cmake_args = [
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + build_directory,
+        ]
+        
+        build_args = []
+        cmake_args += []
+
+        # Assuming Makefiles
+        build_args += ['--', '-j4']
+        self.build_args = build_args
+        
+        env = os.environ.copy()
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        # CMakeLists.txt is in the same directory as this setup.py file
+        cmake_list_dir = os.path.abspath(os.path.dirname(__file__))
+        print('-' * 10, 'Running CMake prepare', '-' * 40)
+        
+        subprocess.check_call(['cmake', cmake_list_dir] + cmake_args,
+                              cwd=self.build_temp,
+                              env=env)
+
+        print('-' * 10, 'Building extensions', '-' * 40)
+        
+        cmake_cmd = ['cmake', '--build', '.'] + self.build_args
+        subprocess.check_call(cmake_cmd,
+                              cwd=self.build_temp)
+
+        for ext in self.extensions:
+            self.copy_ext(ext.name)
+            
+    def copy_ext(self, ext_name):
+        # Move from build temp to final position
+        build_temp = Path(self.build_temp).resolve()
+        ext_local_path = ext_name + ".so"
+        ext_dir = os.path.abspath(os.path.dirname(
+            self.get_ext_fullpath(ext_name)))
+        dest_path = Path(ext_dir).resolve() / ext_local_path
+        source_path = build_temp / ext_local_path
+        dest_directory = dest_path.parents[0]
+        dest_directory.mkdir(parents=True, exist_ok=True)
+
+        self.copy_file(source_path, dest_path)
+
+        
 setup(
-  name='rodentia',
+    name='rodentia',
 
-  version='0.1.4',
+    version='0.1.4',
 
-  description='3D reinforcement learning platform',
-  long_description="Rodentia is a 3D reinforcement learning platform.",
+    description='3D reinforcement learning platform',
+    long_description="Rodentia is a 3D reinforcement learning platform.",
 
-  url='https://github.com/miyosuda/rodentia',
+    url='https://github.com/miyosuda/rodentia',
 
-  author='Kosuke Miyoshi',
-  author_email='miyosuda@gmail.com',
+    author='Kosuke Miyoshi',
+    author_email='miyosuda@gmail.com',
 
-  install_requires=['numpy(>=1.11.0)'],
-  packages=['rodentia'],
-  package_data={'rodentia': ['rodentia_module.so']},
+    install_requires=['numpy(>=1.11.0)'],
+    packages=['rodentia'],
+    package_data={'rodentia': ['rodentia_module.so']},
 
-  license='Apache 2.0',
+    license='Apache 2.0',
 
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'License :: OSI Approved :: Apache Software License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: Apache Software License',
 
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-  ],
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 
-  # What does your project relate to?
-  keywords=['rodentia', 'ai', 'deep learning', 'reinforcement learning', 'research'],
+    # What does your project relate to?
+    keywords=['rodentia', 'ai', 'deep learning', 'reinforcement learning', 'research'],
+
+    ext_modules=[
+        CMakeExtension('rodentia/rodentia_module'),
+    ],
+
+    cmdclass={
+        'build_ext' : CMakeBuild
+    },
+
+    zip_safe=False,
 )
