79b869237dc4c7bcfa1037e0a8f4af9086bc37df
kosuke miyoshi <miyoshi@narr.jp>
2020-03-11 17:24:04 +0900

    remove python2 compatibility code

--- examples/01_seekavoid_arena/main.py
+++ examples/01_seekavoid_arena/main.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import numpy as np
 import pygame, sys
 from pygame.locals import *
