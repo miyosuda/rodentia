deb454d260de421753e95274b652b22d309a6985
kosuke miyoshi <miyoshi@narr.jp>
2020-03-15 18:17:55 +0900

    add API document generated by pdoc

--- rodentia/__init__.py
+++ rodentia/__init__.py
@@ -1,4 +1,4 @@
-from rodentia.core import Environment, MultiAgentEnvironment
+from rodentia.core import Environment, MultiAgentEnvironment, BaseEnvironment
 from rodentia.__version__ import __version__
 
-__all__ = ["Environment", "MultiAgentEnvironment"]
+__all__ = ["Environment", "MultiAgentEnvironment", "BaseEnvironment"]
