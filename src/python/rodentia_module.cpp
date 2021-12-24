3d60a234eecd5e2476b186365eb121f0f3655c2e
kosuke miyoshi <miyosuda@gmail.com>
2021-12-24 23:51:01 +0900

    update version

--- src/python/rodentia_module.cpp
+++ src/python/rodentia_module.cpp
@@ -14,7 +14,7 @@ using namespace std;
 #include "Matrix4f.h"
 #include "EnvironmentObject.h"
 
-#define RODENTIA_MODULE_VERSION "0.0.8"
+#define RODENTIA_MODULE_VERSION "0.0.9"
 
 
 //---------------------------------------------------------
